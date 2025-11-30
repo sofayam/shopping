d0bb3a6c9210e062c25de172ec7b793c54ce41ba
mark andrew <mark@air.local>
2025-11-30 20:52:33 +0000

    regex bug

--- server/server.js
+++ server/server.js
@@ -149,7 +149,6 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
 
-
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
