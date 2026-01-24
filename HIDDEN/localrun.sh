a2e1be04d0818cb3ab726f06e49a6466c7717be4
mark andrew <mark@air.local>
2026-01-24 13:26:23 +0000

    hidden

---
+++ HIDDEN/localrun.sh
@@ -0,0 +1,4 @@
+export SERVER_PORT=3001
+export CLIENT_PORT=3000
+node server/server.js
+npm --prefix client start
--- HIDDEN/server/server.js
+++ HIDDEN/server/server.js
@@ -6,7 +6,7 @@ const path = require('path');
 const { v4: uuidv4 } = require('uuid');
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.SERVER_PORT || 3001;
 
 const DATA_DIR = path.join(__dirname, 'data');
 const CATALOG_PATH = path.join(DATA_DIR, 'catalog.json');
